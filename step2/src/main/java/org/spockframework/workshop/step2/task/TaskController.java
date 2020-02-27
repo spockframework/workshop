4e7c52a2580b6f2c19c60f5311a2051c7b5c7c78
Leonard Brünings <leonard.bruenings@1und1.de>
2020-02-27 17:19:46 +0100

    Add @-Date Pattern support

--- step2/src/main/java/org/spockframework/workshop/step2/task/TaskController.java
+++ step2/src/main/java/org/spockframework/workshop/step2/task/TaskController.java
@@ -1,13 +1,21 @@
 package org.spockframework.workshop.step2.task;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.lang.NonNull;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
 
 import java.util.List;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(path = "/tasks", produces = APPLICATION_JSON_VALUE)
@@ -15,7 +23,7 @@ public class TaskController {
 
     private final TaskService taskService;
 
-    TaskController(final TaskService taskService) {
+    TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
@@ -42,15 +50,23 @@ public class TaskController {
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
-    ResponseEntity<Task> postTask(@RequestBody final Task task, UriComponentsBuilder uriComponentsBuilder) {
+    ResponseEntity<Task> postTask(@RequestBody Task task, UriComponentsBuilder uriComponentsBuilder) {
         Task createdTask = taskService.saveTask(task);
         return ResponseEntity
                 .created(uriComponentsBuilder.path("tasks/{id}").buildAndExpand(createdTask.getId()).toUri())
-                .body(taskService.saveTask(task));
+                .body(taskService.saveTask(task)); // TODO - intentional
+    }
+
+    @PostMapping(consumes = TEXT_PLAIN_VALUE)
+    ResponseEntity<Task> postPlainTask(@RequestBody String task, UriComponentsBuilder uriComponentsBuilder) {
+        Task createdTask = taskService.createTaskFromPlainText(task);
+        return ResponseEntity
+                .created(uriComponentsBuilder.path("tasks/{id}").buildAndExpand(createdTask.getId()).toUri())
+                .body(createdTask);
     }
 
     @PatchMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE)
-    ResponseEntity<Task> patchTask(@PathVariable @NonNull String id, @RequestBody final Task update) {
+    ResponseEntity<Task> patchTask(@PathVariable @NonNull String id, @RequestBody Task update) {
         return taskService.updateTask(id, update)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -59,6 +75,6 @@ public class TaskController {
     @DeleteMapping(path = "/{id}")
     ResponseEntity<Void> deleteTask(@PathVariable @NonNull String id) {
         taskService.deleteTask(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
--- step2/src/main/java/org/spockframework/workshop/step2/task/TaskService.java
+++ step2/src/main/java/org/spockframework/workshop/step2/task/TaskService.java
@@ -1,19 +1,44 @@
 package org.spockframework.workshop.step2.task;
 
-import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Service;
-
+import java.time.Clock;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
 
 @Service
 class TaskService {
 
+    private static final Pattern DOUBLE_NEWLINE = Pattern.compile("\r?\n\r?\n");
+
+    private static final Pattern DUE_DATE_PATTERN = Pattern.compile("(?:^|\\s)@(?:"
+                    + "(?<keyword>today|tomorrow)|"
+                    + "next(?<next>monday|tuesday|wednesday|thursday|friday|saturday|sunday|week|month)|"
+                    + "(?<date>\\d{4}-\\d{2}-\\d{2})"
+                    + ")(?:$|\\s)",
+            Pattern.CASE_INSENSITIVE);
+
     private final TaskRepository taskRepository;
 
-    TaskService(final TaskRepository taskRepository) {
+    private final Clock clock;
+
+    @Autowired
+    TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+        this.clock = Clock.systemDefaultZone();
+    }
+
+    TaskService(TaskRepository taskRepository, Clock clock) {
+        this.taskRepository = taskRepository;
+        this.clock = clock;
     }
 
     List<Task> findTasksToDo() {
@@ -21,22 +46,22 @@ class TaskService {
     }
 
     List<Task> findTasksOverdue() {
-        return taskRepository.findAllByDoneIsFalseAndDueDateBefore(LocalDate.now());
+        return taskRepository.findAllByDoneIsFalseAndDueDateBefore(LocalDate.now(clock));
     }
 
     List<Task> findTasksDueToday() {
-        return taskRepository.findAllByDoneIsFalseAndDueDate(LocalDate.now());
+        return taskRepository.findAllByDoneIsFalseAndDueDate(LocalDate.now(clock));
     }
 
-    Task saveTask(final Task task) {
+    Task saveTask(Task task) {
         return taskRepository.save(task);
     }
 
-    Optional<Task> findTask(@NonNull final String id) {
+    Optional<Task> findTask(@NonNull String id) {
         return taskRepository.findById(id);
     }
 
-    Optional<Task> updateTask(@NonNull final String id, @NonNull Task update) {
+    Optional<Task> updateTask(@NonNull String id, @NonNull Task update) {
         return taskRepository.findById(id).map(task -> {
             if (update.getTitle() != null) {
                 task.setTitle(update.getTitle());
@@ -54,7 +79,67 @@ class TaskService {
         });
     }
 
-    void deleteTask(String id) {
+    void deleteTask(@NonNull String id) {
         taskRepository.deleteById(id);
+    }
+
+    Task createTaskFromPlainText(@NonNull String input) {
+        Task task = parseTask(input);
+        return taskRepository.save(task);
+    }
+
+    private Task parseTask(@NonNull String input) {
+        Task task = new Task();
+        task.setDueDate(parseDueDate(input));
+        String[] split = DOUBLE_NEWLINE.split(input, 2);
+        task.setTitle(split.length == 2 ? split[0] : null);
+        task.setDescription(split.length == 2 ? split[1] : split[0]);
+        return task;
+    }
+
+    @Nullable
+    private LocalDate parseDueDate(@NonNull String input) {
+        Matcher matcher = DUE_DATE_PATTERN.matcher(input);
+        if (matcher.find()) {
+            LocalDate result = null;
+            String date = matcher.group("date");
+            String keyword = matcher.group("keyword");
+            String next = matcher.group("next");
+            if (date != null) {
+                result = LocalDate.parse(date);
+            } else if (keyword != null) {
+                switch (keyword.toLowerCase()) {
+                case "today":
+                    result = LocalDate.now(clock);
+                    break;
+                case "tomorrow":
+                    result = LocalDate.now(clock).plusDays(1);
+                    break;
+
+                default:
+                    throw new IllegalStateException("This should never happen(tm)");
+                }
+            } else if (next != null) {
+                switch (next.toLowerCase()) {
+                case "week":
+                    result = LocalDate.now(clock).with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+                    break;
+                case "month":
+                    result = LocalDate.now(clock).with(TemporalAdjusters.firstDayOfNextMonth());
+                    break;
+
+                default:
+                    DayOfWeek dayOfWeek = DayOfWeek.valueOf(next.toUpperCase());
+                    result = LocalDate.now(clock).with(TemporalAdjusters.next(dayOfWeek));
+                    break;
+                }
+            }
+
+            if (matcher.find()) {
+                throw new IllegalArgumentException("Only one @ pattern occurrence is supported.");
+            }
+            return result;
+        }
+        return null;
     }
 }
